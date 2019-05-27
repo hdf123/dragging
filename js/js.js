a03064143ac109d5b5146c99d2c0fbb6ab801b37
thbts <1205552014@qq.com>
2019-05-27 16:34:51 +0800

    000

---
+++ js/js.js
@@ -0,0 +1,7 @@
+//css
+document.write('<link rel="stylesheet" type="text/css" href="css/base.css"/>');
+document.write('<link rel="stylesheet" type="text/css" href="css/layouts.css"/>');
+
+
+//js
+document.write('<script src="js/flexible.js"><\/script>');
